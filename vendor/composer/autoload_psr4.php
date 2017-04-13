06c3f4bf145e90b21fd944d341959a1dc00bfb85
Slaven Sakačić <slaven.sakacic@gauss.hr>
2017-04-13 13:09:17 +0200

    New

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -6,5 +6,5 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
-    'slavensaka\\mypackage\\' => array($baseDir . '/packages/slavensaka/mypackage/src', $baseDir . '/tests'),
+    'slavensaka\\mypackage\\' => array($baseDir . '/src', $baseDir . '/tests'),
 );
